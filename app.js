146aed8edc18fdca2505665632aee94273907b72
masp1234 <mart2c04@stud.kea.dk>
2023-02-26 18:15:21 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+app.use(express.static('public'));
+
+
+app.get("/", (req, res) => {
+    res.sendFile('public/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+})
