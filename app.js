2d262622aea9168a42056c4790458d9e055f5dcd
masp9 <mart2c04@stud.kea.dk>
2023-02-27 15:17:36 +0100

    small change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const PORT = 8080;
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/'));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
